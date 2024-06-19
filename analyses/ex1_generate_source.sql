94e2075827d8bee4f92542b80109ac753aca33f4
Cecile Chow <cecile.chow@gmail.com>
2024-06-19 20:30:09 +0000

    add staging models and codegen examples

---
+++ analyses/ex1_generate_source.sql
@@ -0,0 +1,7 @@
+-- Generate all the tables in schema
+{{ codegen.generate_source(schema_name= 'jaffle_shop', database_name= 'dbt-tutorial') }}
+
+==========================================================================
+
+-- Generate with descriptions and alternative shema name (Preferred)
+{{ codegen.generate_source(schema_name= 'stripe', database_name= 'dbt-tutorial', include_schema=True, name='alt_schema_name', include_descriptions=True) }}
